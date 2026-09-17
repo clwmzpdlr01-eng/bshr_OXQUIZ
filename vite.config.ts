e1a6e10883540d88d8423ee480c32e6aa40b2078
clwmzpdlr01-eng <clwmzpdlr01@gmail.com>
2026-09-17 16:29:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Tailwind v4 플러그인 연결
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
